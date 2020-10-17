236c6c215be5af7cc6319568f590299ac1ce57fa
Alex Wong <alexwbtg@gmail.com>
2020-10-17 21:51:22 +0800

    updated gpio test

--- raspberry-pi/gpio-test.js
+++ raspberry-pi/gpio-test.js
@@ -1,6 +1,6 @@
 const { Gpio } = require('pigpio');
 
-const pin = new Gpio(36, { mode: Gpio.OUTPUT });
+const pin = new Gpio(16, { mode: Gpio.OUTPUT });
 
 module.exports = {
     on: () => pin.digitalWrite(1),
